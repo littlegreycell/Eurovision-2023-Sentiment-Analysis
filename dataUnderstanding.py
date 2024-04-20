482b81c1a7639a8bcffefe83b7686b7fa16de9c1
littlegreycell <167465336+littlegreycell@users.noreply.github.com>
2024-04-20 22:52:12 +0100

    Create dataUnderstanding.py
    Dataunderstanding process was applied to the data sets.

---
+++ dataUnderstanding.py
@@ -0,0 +1,11 @@
+# Before the Eurovision Final
+dataBefore = twitterDataBefore
+dataBefore.shape
+dataBefore.info()
+dataBefore.head()
+
+# After the Eurovision Final
+dataAfter = twitterDataAfter
+dataAfter.shape
+dataAfter.info()
+dataAfter.head()
